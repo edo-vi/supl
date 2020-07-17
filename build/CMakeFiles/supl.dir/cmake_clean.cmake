2930b400942a2ff056c045412240b807edda7c97
Edoardo Z <edovi@icloud.com>
2020-07-17 23:58:01 +0200

    removes files from build

--- build/CMakeFiles/supl.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "libsupl.pdb"
-  "libsupl.a"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/supl.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
